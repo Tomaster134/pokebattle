ab76a6ec04c2f2f1996c930b625bc2a040ac708b
Tomaster134 <tomaster134@gmail.com>
2024-02-16 23:29:51 -0500

    I found the bug. Trumpets sound and the heavens tremble.

--- app/blueprints/battle/routes.py
+++ app/blueprints/battle/routes.py
@@ -106,6 +106,8 @@ def battle_progress(opp_id):
 # Something in this section causes a continual refresh of battle_progress I think? Render keeps saying GET battle_results and I now have 250 battles in my database. Not sure if it's because I'm using session to store a variable to move between battle_progress and battle_results?
 
 # It's hitting the battle_results page because the result of the victory is being logged in the database, so the variable is being passed between the two routes. Why does it want to circle back though? Is it the URL redirect?
+
+#wasn't even really because of the route. JavaScript just kept resubmitting because it was taking too long to get the next page loaded and it was set to submit a url_for when the counter was less than or equal to 0, so it just kept resubmitting every 1000ms
 @battle.route('/battle_results/<opp_id>')
 @login_required
 def battle_results(opp_id):
